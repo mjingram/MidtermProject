bbb55e1a2edc72df52d54ab211b545acf9cd9061
mjingram <micha.j.ingram@gmail.com>
2021-07-17 13:34:46 -0600

    add junit for brewery test and address

--- JPABuzzFinder/src/test/java/com/skilldistillery/jpabuzzfinder/entities/AddressTest.java
+++ JPABuzzFinder/src/test/java/com/skilldistillery/jpabuzzfinder/entities/AddressTest.java
@@ -2,33 +2,54 @@ package com.skilldistillery.jpabuzzfinder.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AddressTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Address addr;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		
+		emf = Persistence.createEntityManagerFactory("JPABuzzFinder");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		addr = em.find(Address.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-	}
+		addr= null;
+		em.close();	
+		}
+
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("testing address mapping")
+	void test1() {
+		assertNotNull(addr);
+		assertEquals("Minneapolis", addr.getCity());
+		assertEquals("MN", addr.getState());
+		assertEquals("55111", addr.getZipcode());
 	}
 
 }
--- JPABuzzFinder/src/test/java/com/skilldistillery/jpabuzzfinder/entities/BreweryTest.java
+++ JPABuzzFinder/src/test/java/com/skilldistillery/jpabuzzfinder/entities/BreweryTest.java
@@ -1,34 +1,76 @@
 package com.skilldistillery.jpabuzzfinder.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BreweryTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Brewery bt;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		
+		emf = Persistence.createEntityManagerFactory("JPABuzzFinder");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		bt = em.find(Brewery.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-	}
+		bt = null;
+		em.close();	
+		}
 
+	
 	@Test
+	@DisplayName("testing brewery mapping")
 	void test() {
-		fail("Not yet implemented");
+		assertNotNull(bt);
+		assertEquals("NorthGate Brewing", bt.getName());
+		
+		/*SELECT * FROM brewery;
++----+-------------------+------------+-----------+----------------+
+| id | name              | address_id | image_url | logo_image_url |
++----+-------------------+------------+-----------+----------------+
+|  1 | NorthGate Brewing |          1 | NULL      | NULL           |
++----+-------------------+------------+-----------+----------------+
+1 row in set (0.00 sec)
+		 * 
+		 * 
+		 */
 	}
+	
+	@Test
+	@DisplayName("testing brewery to address mapping")
+	void test1() {
+		assertNotNull(bt);
+		assertEquals("Minneapolis", bt.getAddress().getCity());
+		assertEquals("MN", bt.getAddress().getState());
+		assertEquals("55111", bt.getAddress().getZipcode());
+	}
+	
+	
 
 }
