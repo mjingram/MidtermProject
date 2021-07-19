32c9fdf5ead30034aef252864922f59909141240
ren09 <rachel.richardson915@gmail.com>
2021-07-19 14:43:10 -0600

    finish user controller

--- BuzzFinder/src/main/java/com/skilldistillery/jpabuzzfinder/controller/UserController.java
+++ BuzzFinder/src/main/java/com/skilldistillery/jpabuzzfinder/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skilldistillery.jpabuzzfinder.data.UserDAO;
 import com.skilldistillery.jpabuzzfinder.entities.Beer;
+import com.skilldistillery.jpabuzzfinder.entities.Brewery;
 import com.skilldistillery.jpabuzzfinder.entities.User;
 
 @Controller
@@ -15,27 +16,33 @@ public class UserController {
 	@Autowired
 	private UserDAO userDao;
 	
+	
 	@RequestMapping("getUserProfile.do")
 	public String getUserProfile(String username, String password, Model model) {
+		
 		model.addAttribute("user", userDao.login(username, password));
 		
-		return "profile";
+		return "account";
 	}
+	
 	
 	@RequestMapping("createUserProfile.do")
 	public String createUserProfile(User user, Model model) {
+		
 		model.addAttribute("user", userDao.createUser(user));
 		
 		return "profile";
 	}
 	
+	
 	@RequestMapping("updateUserProfile.do")
-	public String updateUserProfile(User user, Model model) {
+	public String updateUserProfile(User user, String password, Model model) {
 		
-		model.addAttribute("user", userDao.deleteUser(user));
+		model.addAttribute("user", userDao.updateUsernameAndPassword(password, user));
 		
 		return "profile";
 	}
+	
 	
 	@RequestMapping("deleteUserProfile.do")
 	public String deleteUserProfile(User user, Model model) {
@@ -45,11 +52,39 @@ public class UserController {
 		return "profile";
 	}
 	
-	@RequestMapping("AddFavoriteBeer.do")
+	
+	@RequestMapping("addFavoriteBeer.do")
 	public String addFavoriteBeer(Beer beer, User user, Model model) {
+		
 		model.addAttribute("beer", userDao.addFavBeer(user, beer));
 		
 		return "profile";
 	}
+	
+	
+	@RequestMapping("addFavoriteBrewery.do")
+	public String addFavoriteBrewery(Brewery brewery, User user, Model model) {
+		
+		model.addAttribute("beer", userDao.addFavBrewery(user, brewery));
+		
+		return "profile";
+	}
 
+	
+	@RequestMapping("removeFavoriteBeer.do")
+	public String removeFavoriteBeer(Beer beer, User user, Model model) {
+		
+		model.addAttribute("beer", userDao.removeFavBeer(user, beer));
+		
+		return "profile";
+	}
+	
+	
+	@RequestMapping("removeFavoriteBrewery.do")
+	public String removeFavoriteBrewery(Brewery brewery, User user, Model model) {
+		
+		model.addAttribute("beer", userDao.removeFavBrewery(user, brewery));
+		
+		return "profile";
+	}
 }
